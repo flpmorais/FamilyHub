87c4b04ee4446e5e6b62cc0882e51fa68215afa2
Filipe Morais <fmorais@unknownf0b61e574036.lan>
2026-03-22 17:10:58 +0000

    restart

--- components/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all shared components
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Card } from './Card';
-export { default as LoadingSpinner } from './LoadingSpinner';
